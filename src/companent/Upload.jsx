8a0b6cb9e512e7c442310c673f18ae95c353d6a2
sangjuneeeee <wns1169@gmail.com>
2023-05-05 16:40:05 +0900

    feature : Upload 의 기능을 추가한다. - Upload를 하기 위한 버튼을 생성한다.

---
+++ src/companent/Upload.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Upload.css';
+function Upload() {
+
+    //업로드 버튼 생성
+        return (
+            <button className="button-wrapper"></button>
+        );
+    };
+
+export default Upload;
