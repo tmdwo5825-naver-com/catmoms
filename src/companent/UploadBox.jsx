2ad208b9a373509899c9c751a0f70161cea84f59
sangjuneeeee <wns1169@gmail.com>
2023-05-07 19:29:17 +0900

    refactor : Upload의 기능을 개선한다 -Upload 버튼 오버레이 기능을 추가 컴포넌트로 연결하여 개선한다.

--- src/companent/UploadBox.jsx
+++ src/companent/UploadBox.jsx
@@ -1,23 +1,28 @@
-import React from "react";
-import classes from "./Upload.module.css";
+import React, {useState} from "react";
+import classes from "./UploadBox.module.css";
 
-function inputUpload({onClose}){
+function InputUpload(){
+    // textbox에 입력받은 것을 저장해주는 함수 구현
+    const [enteredText, setEnteredText] = useState('')
+    function changeTextHandler(event){
+        setEnteredText(event.target.value);
+
+    }
+    //업로드 버튼 클릭시 생성되는 박스
     return (
         <form className={classes.form}>
-            <button onClick={onClose} className={classes.closeButton}>
-                X
-            </button>
             <p>
                 <label htmlFor="name">Upload Cat Image</label>
                 <input type={"file"}/>
             </p>
             <p>
                 <label htmlFor="name">This Cat Is</label>
-                <input type="text" id="body" required rows="1" placeholder="Explain your CAT"></input>
+                {/*onChange={changeTextHandler}는 textbox에 입력된 것을 받아주는 함수*/}
+                <input type="text" id="body" required rows="1" placeholder="Explain your CAT" onChange={changeTextHandler}></input>
             </p>
-                <button>제출</button>
+                <button >제출</button>
             </form>
     );
 };
 
-export default inputUpload;
+export default InputUpload;
