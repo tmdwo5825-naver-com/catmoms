b64474b3cbf97d6222946ac3c0374a2f0b40966e
sangjuneeeee <wns1169@gmail.com>
2023-05-15 03:32:35 +0900

    refactor:RootLayout.jsx에서 불필요한 부분을 제거한다.

--- src/companent/routes/MainHeader.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function MainHeader(){
-
-    return  <h1>Mr.CatMOM</h1>
-}
-
-export default MainHeader;
--- src/companent/routes/RootLayout.jsx
+++ src/companent/routes/RootLayout.jsx
@@ -1,14 +1,10 @@
 import {Outlet} from 'react-router-dom'
-import MainHeader from "./MainHeader";
-import Upload from "./Upload";
 import React from "react";
-import ToShowAll from "../ToShowAll";
+
 
 function RootLayout(){
     return(
         <>
-            <MainHeader />
-            <Upload/>
             <Outlet />
         </>
     )
